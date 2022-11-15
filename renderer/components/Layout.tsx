1ce8538e3acf466f4d12b7a92205d54c7e6288ca
jafb321 <jafb321@gmail.com>
2022-11-15 02:04:23 -0700

    feat: csv plotter chart

--- renderer/components/Layout.tsx
+++ renderer/components/Layout.tsx
@@ -14,21 +14,6 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/initial-props">
-          <a>With Initial Props</a>
-        </Link>
-      </nav>
-    </header>
     {children}
     <footer>
       <hr />
--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,29 +1,129 @@
-import { useEffect, useState } from 'react'
-import Link from 'next/link'
+import { ChangeEventHandler, useEffect, useState } from 'react'
 import Layout from '../components/Layout'
 
-const IndexPage = () => {
-  const [files, setFiles] = useState<String[]>([]);
-  
-  useEffect(() => {
-    setFiles(window.files.getFiles());
-  }, [])
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { csvData } from '../interfaces';
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  interaction: {
+    mode: 'index' as const,
+    intersect: false,
+  },
+  stacked: false,
+  plugins: {
+    title: {
+      display: true,
+      text: 'CSV Plotter',
+    },
+  },
+  scales: {
+    y: {
+      type: 'linear' as const,
+      display: true,
+      position: 'left' as const,
+    },
+    y1: {
+      type: 'linear' as const,
+      display: true,
+      position: 'right' as const,
+      grid: {
+        drawOnChartArea: false,
+      },
+    },
+  },
+};
+
+const IndexPage = () => {
+  const [file, setFile] = useState("");
+  const [csvData, setCsvData] = useState<null | csvData>(null);
+
+  useEffect(() => {
+    (async () => {
+
+      if (file === "") return
+      setInterval(async () => {
+        const csv = await window.files.getDataFromCsv(file)
+        setCsvData(csv)
+      }, 1000)
+
+    })()
+  }, [file])
+
+
+  const onFileChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+
+    let selectedFile = e.target.files[0];
+    setFile(selectedFile.path)
+
+  };
+
+  const data = {
+    labels: csvData?.timestamp,
+    datasets: [
+      {
+        label: 'Temperatura',
+        data: csvData?.temperature,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        yAxisID: 'y',
+      },
+      {
+        label: 'Humedad',
+        data: csvData?.humidity,
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        yAxisID: 'y1',
+      },
+    ],
+  };
 
 
   return (
-    <Layout title="Home | Next.js + TypeScript + Electron Example">
-      <h1>Hello Next.js 👋</h1>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
+    <>
+      {!csvData ?
+        <Layout title="Home | Next.js + TypeScript + Electron Example">
+          <div className="h-screen flex justify-center items-center bg-gray-900 px-2">
+            {file}
+            <div className="p-3 md:w-1/2 w-[360px] rounded-md">
+              <div className="h-32 w-full overflow-hidden relative shadow-md border-2 items-center rounded-md cursor-pointer   border-gray-400 border-dotted">
+                <input type="file" onChange={onFileChange} className="h-full w-full opacity-0 z-10 absolute" name="files[]" />
+                <div className="h-full w-full bg-gray-200 absolute z-1 flex justify-center items-center top-0">
+                  <div className="flex flex-col">
+                    <i className="mdi mdi-folder-open text-[30px] text-gray-400 text-center"></i>
+                    <span className="text-[12px]">{`Drag and Drop a file`}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
 
-        {files.map(file => (
-          <p>{file}</p>
-        ))}
-      </p>
-    </Layout>
+
+        </Layout> :
+
+        <Line options={options} data={data} />
+      }
+    </>
   )
 }
 
